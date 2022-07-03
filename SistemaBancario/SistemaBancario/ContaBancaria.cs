c17dd2a70f9f51c07d499ca3782d2fefdad33cca
Matheus Peluso <matheuspeluso17@gmail.com>
2022-07-03 12:10:09 -0300

    sistema bancario simples v1

---
+++ SistemaBancario/SistemaBancario/ContaBancaria.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace SistemaBancario
+{
+    class ContaBancaria
+    {
+        public int NumeroConta { get; private set; }
+        public double Saldo { get; private set;}
+        public string NomeTitular;
+        public double Taxa = 5.00;
+
+        public ContaBancaria()
+        {
+
+        }
+        
+        public ContaBancaria(int numero, string titular)
+        {
+            NumeroConta = numero;
+            NomeTitular = titular;
+        }
+
+        public ContaBancaria(int numero, double valor, string titular) : this(numero, titular)
+        {
+            Deposito(valor);
+        }
+
+        // OPERAÇÕES
+        public void Deposito(double valorDeposito)
+        {
+            Saldo += valorDeposito; // += É A MESMA COISA QUE : Saldo = Saldo + valorDeposito;
+        }
+
+        public void Saque(double valorSaque)
+        {
+            Saldo = Saldo - valorSaque - Taxa;  // mesma coisa que : Saldo -= (valorSaque + Taxa);
+        }
+
+        public override string ToString()
+        {
+            return "Conta "+ NumeroConta +", Titular: "+NomeTitular+", Saldo: R$ "+ Saldo.ToString("f2",CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ SistemaBancario/SistemaBancario/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+
+namespace SistemaBancario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria _contaBancaria;
+            Console.Write("Entre com o número da conta: ");
+            int accNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do titular da conta: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("Haverá deposito inicial (s/n) ? ");
+            string resp = Console.ReadLine();
+            double deposito_saque = 0;
+            if (resp == "s")
+            {
+                Console.Write("Entre com o valor de depósito inicial: ");
+                deposito_saque = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                _contaBancaria = new ContaBancaria(accNumber, deposito_saque, name);
+
+                
+            }
+            else
+            {
+                _contaBancaria=new ContaBancaria(accNumber, name);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(_contaBancaria);
+
+            Console.Write("Você deseja fazer Depósito (s/n) ?  ");
+            resp = Console.ReadLine();
+
+            if (resp == "s")
+            {
+                Console.Write("Entre com o valor do depósito: ");
+                deposito_saque = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                _contaBancaria.Deposito(deposito_saque);
+
+                Console.WriteLine();
+                Console.WriteLine("Dados Atualizados: ");
+                Console.WriteLine(_contaBancaria);
+            }
+
+            Console.Write("Você deseja fazer Saque (s/n) ?");
+            resp = Console.ReadLine();
+            if (resp == "s")
+            {
+                Console.Write("Entre com o valor que deseja sacar: ");
+                deposito_saque = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                _contaBancaria.Saque(deposito_saque);
+
+                Console.WriteLine();
+                Console.WriteLine("Dados Atualizados: ");
+                Console.WriteLine(_contaBancaria);
+
+            }
+        }
+    }
+}
